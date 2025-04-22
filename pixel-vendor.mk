c80372f5bd292768afe0a1cad3c8ae43b3296ed0
NurKeinNeid <nurkeinneid@derpfest.org>
2025-04-22 16:43:02 +0200

    pixel: Update clocks from caiman-BP1A.250305.020
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- pixel-vendor.mk
+++ pixel-vendor.mk
@@ -107,11 +107,9 @@ PRODUCT_PACKAGES += \
     Flipendo \
     SetupWizardPixelPrebuilt \
     SystemUIClocks-BigNum \
-    SystemUIClocks-BigNum2 \
     SystemUIClocks-Calligraphy \
     SystemUIClocks-Flex \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
-    SystemUIClocks-NumOverlap \
-    SystemUIClocks-Weather
+    SystemUIClocks-NumOverlap
